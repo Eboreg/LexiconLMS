5950f803c1166322a3112303f0a2a6d872b13fb1
Robert Huselius <robert@huseli.us>
2018-07-18 14:32:50 +0200

    Added fist/last names for user, seeding Roles

--- LexiconLMS/Migrations/Configuration.cs
+++ LexiconLMS/Migrations/Configuration.cs
@@ -1,18 +1,24 @@
+using Microsoft.AspNet.Identity.EntityFramework;
+
 namespace LexiconLMS.Migrations
 {
     using System.Data.Entity.Migrations;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<LexiconLMS.Models.ApplicationDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<Models.ApplicationDbContext>
     {
         public Configuration() {
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(LexiconLMS.Models.ApplicationDbContext context) {
+        protected override void Seed(Models.ApplicationDbContext context) {
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
+            context.Roles.AddOrUpdate(r => new { r.Name },
+                new IdentityRole { Name = "Teacher" },
+                new IdentityRole { Name = "Student" }
+            );
         }
     }
 }
--- LexiconLMS/Models/ApplicationUser.cs
+++ LexiconLMS/Models/ApplicationUser.cs
@@ -7,6 +7,10 @@ namespace LexiconLMS.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName => FirstName + " " + LastName;
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager) {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
